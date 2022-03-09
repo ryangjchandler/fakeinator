f2a5fc8ea15763fedc8f0effb6e555374b663a1c
Ryan Chandler <ryangjchandler@gmail.com>
2022-03-09 15:40:21 +0000

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Fakeinator
+
+A super tiny webpage for generating data with [Faker.js](https://fakerjs.dev/) and exporting as a CSV.
+
+## Usage
+
+Visit the [Fakeinator website]() and select your column names and data types.
+
+Once you've chosen your column names and data types, you can export the data as a CSV directly in the browser.
---
+++ app.js
@@ -0,0 +1,72 @@
+import { faker } from 'https://cdn.skypack.dev/@faker-js/faker';
+
+console.log(faker)
+
+document.addEventListener('alpine:init', () => {
+    Alpine.data('fakeinator', () => ({
+        name: '',
+        type: '',
+        columns: [],
+        rows: 10,
+        optgroups: {},
+        locales: Object.keys(faker.locales),
+        locale: 'en_US',
+        init() {
+            this.optgroups = Object.keys(faker)
+                .filter(g => !['definitions', 'fake', 'unique'].includes(g))
+                .reduce((groups, group) => {
+                    groups[group] = Object.keys(faker[group]).filter(key => ! ['faker', 'Helpers'].includes(key))
+                    return groups
+                }, {})
+        },
+        add() {
+            this.columns.push({ group: this.type.split('|')[0], name: this.name, type: this.type.split('|')[1] })
+            
+            this.name = ''
+            this.type = ''
+        },
+        remove(i) {
+            this.columns = this.columns.filter((_, j) => j !== i)
+        },
+        generate() {
+            if (this.columns.length <= 0) {
+                window.alert('Please add some columns first.')
+                return
+            }
+
+            faker.setLocale(this.locale)
+
+            const rows = [
+                [...this.columns.map(c => c.name)],
+            ]
+
+            try {
+                for (let i = 0; i < this.rows; i++) {
+                    let row = []
+    
+                    for (let j = 0; j < this.columns.length; j++) {
+                        const col = this.columns[j]
+
+                        const fn = faker[col.group][col.type]
+                        row.push(fn.call())
+                    }
+    
+                    rows.push(row)
+                }
+            } catch (e) {
+                window.alert('Oh dear, something went wrong! Check the console and let me know on GitHub :^)')
+
+                throw e;
+            }
+
+            const csvContent = encodeURI('data:text/csv;charset=utf8,' + rows.map(row => row.join(',')).join('\n'))
+            
+            // Hacky downloads...
+            const a = document.createElement('a')
+            a.setAttribute('href', csvContent)
+            a.setAttribute('download', 'faker.csv')
+            document.body.appendChild(a)
+            a.click()
+        }
+    }))
+})
